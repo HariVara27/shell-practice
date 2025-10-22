2a63bef1d5437c139dff8cd0c6ed9dd643f4db63
Hari Vara <harivaraprasad7@yahoo.com>
2025-10-22 10:26:34 +0530

    Variables

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#no space befor or after equalsto.
+person1=John
+peroson2=Raheem
+
+echo "$person1:: Hi $person2, How are you?"
+echo "$person2:: Hello $person1, I'm fine. How are you?"
+echo "$person1:: What are you learning recently?"
+echo "$person2:: I'm learning Shell Scripting $person1"
---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+
+echo "$PERSON1:: welcome to alsaka Putin"
+echo "$PERSON2:: Thank you $PERSON1, finally not on zoom webinar"
+echo "$PERSON1:: So, do you have a deal"
+echo "$PERSON1:: Yes, if you make ukraine joins Russia"
