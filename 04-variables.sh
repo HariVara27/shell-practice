bc8797f3f5ea8fa8e79dd5de432ece06ee34ee7e
Hari Vara <harivaraprasad7@yahoo.com>
2025-10-22 10:38:06 +0530

    Variables

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+#ARGUMENTS
 PERSON1=$1
 PERSON2=$2
 
 
-echo "$PERSON1:: welcome to alsaka Putin"
+echo "$PERSON1:: welcome to alsaka $PERSON2"
 echo "$PERSON2:: Thank you $PERSON1, finally not on zoom webinar"
 echo "$PERSON1:: So, do you have a deal"
 echo "$PERSON1:: Yes, if you make ukraine joins Russia"
