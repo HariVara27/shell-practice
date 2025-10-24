e6a8939a7c3631e8c8ec4846a847459390c2fac6
Hari Vara <harivaraprasad7@yahoo.com>
2025-10-24 08:38:27 +0530

    variables

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -3,3 +3,6 @@
 echo "All variables passed to the script: $@"
 echo "All variables passed to the script: $*"
 echo "Script Name:- $0"
+echo "Current Directory :-$pwd"
+echo "who is running this :-$USER"
+echo "Home Directory of user :: $HOME"
