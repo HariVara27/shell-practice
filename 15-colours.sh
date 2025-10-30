a0b4ebc4a564908581c6a3cf0c0a22e9368c4b61
Hari Vara <harivaraprasad7@yahoo.com>
2025-10-30 09:36:25 +0530

    conditions

--- 15-colours.sh
+++ 15-colours.sh
@@ -14,10 +14,10 @@ fi
 VALIDATE(){ #functions recieve i/p's through args like shell script args
 
     if [ $1 -ne 0 ]; then
-        echo -e "ERROR:: Installing $2 is......$R failure $NO"
+        echo -e "ERROR:: Installing $2 is...... $R failure $NO"
         exit 1
     else
-        echo -e  "Installing $2 is..........$G  SUCCESS $NO"
+        echo -e  "Installing $2 is.......... $G  SUCCESS $NO"
     fi
 
 }
@@ -28,14 +28,14 @@ if [ $? -ne 0 ]; then
     dnf  install mysql -y
     VALIDATE $? "MYSQL"
 else
-    echo "MYSQL already exists.....$Y SKIPPING $NO"
+    echo -e "MYSQL already exists..... $Y SKIPPING $NO "
 fi
 dnf list installed nginx
 if [ $? -ne 0 ]; then
     dnf install nginx -y
     VALIDATE $? "NGINX"
 else
-    echo "NGINX already exists.....$Y SKIPPING $NO"
+    echo -e "NGINX already exists.....$Y SKIPPING $NO"
 fi
 
 dnf list installed nginx
@@ -43,5 +43,5 @@ if [ $? -ne 0 ]; then
     dnf install python3 -y
     VALIDATE $? " python3"
 else
-    echo "Phython already exists.....$Y SKIPPING $NO"
+    echo -e "Phython already exists.....$Y SKIPPING $NO"
 fi
